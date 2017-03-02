02088d89ae732c9ccb43de981f80e457f3955819
Jonathan Helmus <jjhelmus@gmail.com>
2017-03-02 10:56:58 -0600

    Update builds scripts

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
-MINICONDA_URL="http://repo.continuum.io/miniconda"
-MINICONDA_FILE="Miniconda-3.8.3-MacOSX-x86_64.sh"
+MINICONDA_URL="https://repo.continuum.io/miniconda"
+MINICONDA_FILE="Miniconda3-latest-MacOSX-x86_64.sh"
 wget "${MINICONDA_URL}/${MINICONDA_FILE}"
 bash $MINICONDA_FILE -b
 
-export PATH=/Users/travis/miniconda/bin:$PATH
+export PATH=/Users/travis/miniconda3/bin:$PATH
 
 conda update --yes conda
-conda install --yes conda-build jinja2 binstar
-conda config --add channels https://conda.binstar.org/jjhelmus
+conda install --yes conda-build jinja2 anaconda-client
+conda config --add channels jjhelmus
 
 # Remove gfortran so that all fortran modules are not built.
 # This avoids the need to package the gfortran run time in the conda package.
